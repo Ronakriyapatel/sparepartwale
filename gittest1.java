ee8162e4580f9ebafee343801a8314309f0eac9c
cuneiform-ronakpatel <rp6038272@gmail.com>
2024-08-08 18:19:40 +0530

    hello

--- gittest1.java
+++ gittest1.java
@@ -1,6 +1,8 @@
 package Testing;
 
 
+//import java.sql.Driver;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,11 +10,13 @@ public class gittest1
 {
 	public void chrome()
 	{
+		
 		WebDriver driver;
 		System.setProperty("Webdriver.chrome.driver","C:\\Users\\Aaditya\\Selenium3\\chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();	
 		driver.get("https://www.google.com");
+		driver.close();		
 	}
 	
 	
@@ -23,6 +27,8 @@ public class gittest1
 		// TODO Auto-generated method stub
 		gittest1 obj=new gittest1();
 		obj.chrome();
+		
+		
 				
 	}
 
